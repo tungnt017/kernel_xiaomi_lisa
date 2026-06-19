968fc145192705e5c688e95dc4ca19cb812baced
tungnt017 <73889191+tungnt017@users.noreply.github.com>
2026-06-19 16:04:12 +0700

    Add files via upload

--- .github/scripts/fix_resukisu_susfs_magic.py
+++ .github/scripts/fix_resukisu_susfs_magic.py
@@ -16,4 +16,27 @@ orig = data
 # used by this experimental kernel does not export the macro to this file.
 # SUSFS userspace tools conventionally use 0xFAFAFAFA for SUSFS_MAGIC.
 if 'SUSFS_MAGIC' in data and '#define SUSFS_MAGIC' not in data:
-    define_block = 
+    define_block = """
+#ifndef SUSFS_MAGIC
+#define SUSFS_MAGIC 0xFAFAFAFA
+#endif
+"""
+
+    # Prefer placing after local includes, before code. If include section is
+    # hard to detect, place at top; guarded define is safe either way.
+    include_matches = list(re.finditer(r'^#include\s+[<"].*[>"]\s*$', data, re.M))
+    if include_matches:
+        pos = include_matches[-1].end()
+        data = data[:pos] + define_block + data[pos:]
+    else:
+        data = define_block + data
+
+    print('[OK] added guarded SUSFS_MAGIC fallback define to drivers/kernelsu/supercall/supercall.c')
+else:
+    print('[SKIP] SUSFS_MAGIC fallback not needed or already defined')
+
+if data != orig:
+    path.write_text(data)
+    print('[DONE] patched supercall.c SUSFS_MAGIC fallback')
+else:
+    print('[DONE] no changes needed')
